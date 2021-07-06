a6e5e9520dc36872c566a67e0a7b436505cf3ca4
Norbert Schwarz <info@datentechnikschwarz.de>
2021-07-06 20:36:51 +0200

    init

---
+++ inc/import_loader.py
@@ -0,0 +1,8 @@
+def mod_loader():
+    print("Test")
+
+def main():
+    mod_loader()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Main Program
+from inc.import_loader import mod_loader
+
 def main():
     print("Hello World!")
 
