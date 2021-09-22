fa4738b66a9ee738c0c7766aca9b5b0bc2a03d1e
Norbert.Schwarz <info@datentechnikschwarz.de>
2021-09-22 11:17:55 +0200

    init

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Main Program
-from module_loader import mod_loader 
-import module_serial
+import machine
+import time
+#from module_loader import mod_loader 
+#import module_serial
 
+led = machine.Pin(25, machine.Pin.OUT)
 
 
 def main():
-    print("Hello World!!!!")
-    mod_loader()
-
-
-
+    delay_ms = 1000
+    while True:
+        led.toggle()
+        time.sleep_ms(delay_ms)
 
 
 #------------------------------------------------------------------------------
