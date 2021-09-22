decf9d80b8bd222858559b4ff55d78084b18f156
Norbert.Schwarz <info@datentechnikschwarz.de>
2021-09-22 12:54:40 +0200

    init

--- module_loader.py
+++ module_loader.py
@@ -3,11 +3,11 @@ import module_ws2812
 
 def mod_loader():
     print("Start-Loader")
-    # run_ws2812()
+    run_ws2812()
 
 
 def run_ws2812():
-    module_ws2812.mod_ws2812()
+    module_ws2812.run_ws2812()
 
 
 
