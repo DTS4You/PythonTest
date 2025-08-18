6ea72e6f415542c43022bae46efda6cb47b9e04a
Norbert Schwarz <info@datentechnikschwarz.de>
2025-08-18 21:58:44 +0200

    Init

--- finde_eigenschaft_in_objekt.py
+++ finde_eigenschaft_in_objekt.py
@@ -1,11 +1,12 @@
 # Beispielklasse
 class Objekt:
-    def __init__(self, index, stripe, num_led):
+    def __init__(self, index, stripe, num_led, direction=False):
         self.index      = index
         self.stripe     = stripe
         self.num_led    = num_led
         self.start_led  = 0
         self.stop_led   = 0
+        self.direction  = direction
         self.color_off  = (0,0,0)
         
 
@@ -15,24 +16,31 @@ def finde_objekte_mit_eigenschaft(objekte, eigenschaft_wert):
 
 # Beispielobjekte
 objekte = [
-    Objekt(1,1,4),
-    Objekt(2,1,6),
-    Objekt(3,1,8),
-    Objekt(4,2,8)
+    Objekt(1, 1, 4, False),
+    Objekt(2, 1, 6,True),
+    Objekt(3, 1, 8,False),
+    Objekt(4, 2, 8,True),
+    Objekt(5, 2, 8,False)
 ]
 
-# Objekte mit Eigenschaft '1' finden
-ergebnis = finde_objekte_mit_eigenschaft(objekte, 1)
-ergebnis[0].start_led = 5
-for obj in ergebnis:
-    print(obj.index, obj.stripe, obj.num_led, obj.start_led)
+# Objekte mit Eigenschaft 'X' finden
+#ergebnis = finde_objekte_mit_eigenschaft(objekte, 1)
+#for obj in ergebnis:
+#    print(obj.index, obj.stripe, obj.num_led, obj.start_led)
 
 # Alle verschiedenen Eigenschaften ermitteln
 eigenschaften = set(obj.stripe for obj in objekte)
-print(eigenschaften)
 eigenschaften_liste = sorted(eigenschaften)
-print(eigenschaften_liste)
-
+last_index = 0
 for value in eigenschaften_liste:
-    print(value)
+    ergebnis = finde_objekte_mit_eigenschaft(objekte, value)
+    for obj in ergebnis:
+        if value is not last_index:
+            last_index = obj.index
+        else:
+            objekte[obj.index - 1].start_led = objekte[obj.index - 2].num_led + objekte[obj.index - 2].start_led
+        objekte[obj.index - 1].stop_led = objekte[obj.index - 1].start_led + objekte[obj.index - 1].num_led
+        print(obj.index, obj.stripe, obj.start_led, obj.stop_led, obj.num_led, obj.direction)
+
+    
 
