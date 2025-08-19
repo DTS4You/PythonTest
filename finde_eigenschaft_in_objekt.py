3437066962e9ee38bd7bf338b792608d81a688e9
Norbert Schwarz <info@datentechnikschwarz.de>
2025-08-19 11:27:23 +0200

    Init

--- finde_eigenschaft_in_objekt.py
+++ finde_eigenschaft_in_objekt.py
@@ -1,5 +1,5 @@
 # Beispielklasse
-class Objekt:
+class Led_Segment:
     def __init__(self, index, stripe, num_led, direction=False):
         self.index      = index
         self.stripe     = stripe
@@ -15,31 +15,31 @@ def finde_objekte_mit_eigenschaft(objekte, eigenschaft_wert):
     return [obj for obj in objekte if obj.stripe == eigenschaft_wert]
 
 # Objekte anlegen
-objekte = [
-    Objekt(1, 1, 4, False),
-    Objekt(2, 1, 6,True),
-    Objekt(3, 1, 8,False),
-    Objekt(4, 2, 8,True),
-    Objekt(5, 2, 8,False)
+segments = [
+    Led_Segment(1, 1, 4, False),
+    Led_Segment(2, 1, 6,True),
+    Led_Segment(3, 1, 8,False),
+    Led_Segment(4, 2, 8,True),
+    Led_Segment(5, 2, 8,False)
 ]
 
 # Objekte mit Eigenschaft 'X' finden
-#ergebnis = finde_objekte_mit_eigenschaft(objekte, 1)
+#ergebnis = finde_Led_Segment_mit_eigenschaft(objekte, 1)
 #for obj in ergebnis:
 #    print(obj.index, obj.stripe, obj.num_led, obj.start_led)
 
 # Alle verschiedenen Eigenschaften ermitteln
-eigenschaften = set(obj.stripe for obj in objekte)
+eigenschaften = set(obj.stripe for obj in segments)
 eigenschaften_liste = sorted(eigenschaften)
 last_index = 0
 for value in eigenschaften_liste:
-    ergebnis = finde_objekte_mit_eigenschaft(objekte, value)
+    ergebnis = finde_objekte_mit_eigenschaft(segments, value)
     for obj in ergebnis:
         if value is not last_index:
             last_index = obj.index
         else:
-            objekte[obj.index - 1].start_led = objekte[obj.index - 2].num_led + objekte[obj.index - 2].start_led
-        objekte[obj.index - 1].stop_led = objekte[obj.index - 1].start_led + objekte[obj.index - 1].num_led
+            segments[obj.index - 1].start_led = segments[obj.index - 2].num_led + segments[obj.index - 2].start_led
+        segments[obj.index - 1].stop_led = segments[obj.index - 1].start_led + segments[obj.index - 1].num_led
         print(obj.index, obj.stripe, obj.start_led, obj.stop_led, obj.num_led, obj.direction)
 
     
