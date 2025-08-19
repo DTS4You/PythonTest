fdba20fc18639d0befdb7ad710d0718103180979
Norbert Schwarz <info@datentechnikschwarz.de>
2025-08-19 12:28:54 +0200

    Init

--- finde_eigenschaft_in_objekt.py
+++ finde_eigenschaft_in_objekt.py
@@ -1,26 +1,28 @@
-# Beispielklasse
+# Led-Segment Klasse
 class Led_Segment:
-    def __init__(self, index, stripe, num_led, direction=False):
-        self.index      = index
-        self.stripe     = stripe
-        self.num_led    = num_led
-        self.start_led  = 0
-        self.stop_led   = 0
-        self.direction  = direction
+    def __init__(self, uid, stripe, index, num_led, direction=False):
+        self.index      = uid               # LED_Segment UID fortlaufend ab 1
+        self.stripe     = stripe            # LED_Stripe beginnt mit 1 (PIO-Sektion, Pinzuordnung)
+        self.index      = index             # LED_Segemnt im Stripe beginnt mit 1
+        self.num_led    = num_led           # Anzahl der LEDs im Segment
+        self.start_led  = 0                 # Startposition beginnt mit 0
+        self.stop_led   = 0                 # Stopposition beginnt mit 0
+        self.direction  = direction         # Richtung False -> Links nach Rechts / True -> Rechts nach links
         self.color_off  = (0,0,0)
+        self.refresh    = False             # Wurde das Led_Segment verändert
         
 
 # Funktion, um alle Objekte mit einer bestimmten Eigenschaft zu finden
 def finde_objekte_mit_eigenschaft(objekte, eigenschaft_wert):
     return [obj for obj in objekte if obj.stripe == eigenschaft_wert]
 
-# Objekte anlegen
+# Objekte anlegen   (UID, Stripe, Index, Anzahl, Richtung)
 segments = [
-    Led_Segment(1, 1, 4, False),
-    Led_Segment(2, 1, 6,True),
-    Led_Segment(3, 1, 8,False),
-    Led_Segment(4, 2, 8,True),
-    Led_Segment(5, 2, 8,False)
+    Led_Segment(1, 1, 1, 4, False),        # 1. Segment
+    Led_Segment(2, 1, 2, 6, True),         # 2. Segment
+    Led_Segment(3, 1, 3, 8, False),        # 3. Segment
+    Led_Segment(4, 2, 1, 8, True),         # 4. Segment
+    Led_Segment(5, 2, 2, 8, False)         # 5. Segment
 ]
 
 # Objekte mit Eigenschaft 'X' finden
