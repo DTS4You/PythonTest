77acf4993953033814695443b2b9fef5e39302b0
DTS4You <info@datentechnikschwarz.de>
2025-11-25 20:37:24 +0100

    Init

--- main_ws2812_01.py
+++ main_ws2812_01.py
@@ -1,4 +1,7 @@
-# Example showing use of HSV colors
+###############################################################################
+### WS2812 Animation
+### Version: 0.99
+###############################################################################
 import time
 from libs.neopixel import Neopixel
 
@@ -11,7 +14,7 @@ color_default   = (  0,  0,  2)
 
 
 class LED_STRIP:
-    def __init__(self, num_pix, pio_num, pin_num, color_start, color_stop, direction, transfer_mode="PUT"):
+    def __init__(self, num_pix, pio_num, pin_num, color_start, color_stop, direction, anim_type, transfer_mode="PUT"):
         self.num_pix        = num_pix 
         self.pio_num        = pio_num 
         self.pin_num        = pin_num
@@ -22,10 +25,17 @@ class LED_STRIP:
         self.color_value    = ( 40, 40, 40)
         self.anim_count     = 0
         self.anim_enable    = True
-        self.anim_type      = "Pixel"
+        self.anim_type      = anim_type
         self.direction      = direction
+        self.offset         = 8
 
         self.led_setup()
+
+    def set_color_default(self, color):
+        self.color_default = color
+    
+    def set_color_value(self, color):
+        self.color_value = color
 
     def led_setup(self):
         self.strip = (Neopixel(self.num_pix, self.pio_num, self.pin_num, "GRB", 0.0001, self.transfer))
@@ -37,20 +47,12 @@ class LED_STRIP:
         self.strip.fill(self.color_default)
 
     def led_gradient(self):
-        self.strip.set_pixel_line_gradient(0 ,self.num_pix - 1, self.color_start, self.color_stop)
+        self.strip.set_pixel_line_gradient(0, self.num_pix - 1, self.color_start, self.color_stop)
 
     def led_show(self):
         self.strip.show()
 
-    def mask_stripe(self):
-        self.led_gradient()
-        if self.anim_count > 0:
-            self.strip.set_pixel_line(0, self.anim_count - 1 , color_default)
-        if self.anim_count < self.num_pix - 2:
-            self.strip.set_pixel_line(self.anim_count + 3, self.num_pix, color_default)
-    
     def anim_step(self):
-        print("Alt -> " + str(self.anim_count))
         if self.anim_enable:
             if self.direction:
                 if self.anim_count < self.num_pix - 1:
@@ -62,17 +64,26 @@ class LED_STRIP:
                     self.anim_count -= 1
                 else:
                     self.anim_count = self.num_pix - 1
-        #print("Neu -> " + str(self.anim_count))
+
+    def mask_stripe(self):
+        self.led_gradient()
+        if self.anim_count > 0:
+            self.strip.set_pixel_line(0, self.anim_count - 1 , color_default)
+        if self.anim_count < self.num_pix - self.offset - 1:
+            self.strip.set_pixel_line(self.anim_count + self.offset, self.num_pix, color_default)
     
     def make_anim(self):
         if self.anim_type == "Pixel":
             self.led_fill()
             self.led_set_pixel()
+        if self.anim_type == "Mask":
+            self.led_gradient()
+            self.mask_stripe()
         self.anim_step()
 
 
 
-led_1 = LED_STRIP(20, 0, 2, color_red, color_yellow, True)
+led_1 = LED_STRIP(20, 0, 2, color_red, color_yellow, True, "Mask")
 
 
 #led_1.strip_fill()
