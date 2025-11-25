134f4906ca978a503f8247d50e97683bdac5d61c
DTS4You <info@datentechnikschwarz.de>
2025-11-25 20:15:59 +0100

    Init

--- main_ws2812_01.py
+++ main_ws2812_01.py
@@ -2,25 +2,6 @@
 import time
 from libs.neopixel import Neopixel
 
-
-class LED_STRIP:
-    def __init__(self, num_pix, pio_num, pin_num, transfer_mode="PUT"):
-        self.num_pix    = num_pix 
-        self.pio_num    = pio_num 
-        self.pin_num    = pin_num
-        self.transfer   = transfer_mode
-
-    def strip_setup(self):
-        self.strip = (Neopixel(self.num_pix, self.pio_num, self.pin_num, "GRB", 0.0001, self.transfer))
-
-
-led = []
-
-led.append(LED_STRIP(20, 0, 2))
-
-
-
-
 color_off       = (  0,  0,  0)
 color_red       = ( 80,  0,  0)
 color_green     = (  0, 80,  0)
@@ -29,8 +10,80 @@ color_yellow    = ( 50, 50,  0)
 color_default   = (  0,  0,  2)
 
 
-led[0].strip_setup()
+class LED_STRIP:
+    def __init__(self, num_pix, pio_num, pin_num, color_start, color_stop, direction, transfer_mode="PUT"):
+        self.num_pix        = num_pix 
+        self.pio_num        = pio_num 
+        self.pin_num        = pin_num
+        self.transfer       = transfer_mode
+        self.color_default  = ( 0, 0, 5)
+        self.color_start    = color_start
+        self.color_stop     = color_stop
+        self.color_value    = ( 40, 40, 40)
+        self.anim_count     = 0
+        self.anim_enable    = True
+        self.anim_type      = "Pixel"
+        self.direction      = direction
 
-led[0].strip.set_pixel_line(0, 19, color_red)
-led[0].strip.show()
+        self.led_setup()
+
+    def led_setup(self):
+        self.strip = (Neopixel(self.num_pix, self.pio_num, self.pin_num, "GRB", 0.0001, self.transfer))
+
+    def led_set_pixel(self):
+        self.strip.set_pixel(self.anim_count, self.color_value)
+
+    def led_fill(self):
+        self.strip.fill(self.color_default)
+
+    def led_gradient(self):
+        self.strip.set_pixel_line_gradient(0 ,self.num_pix - 1, self.color_start, self.color_stop)
+
+    def led_show(self):
+        self.strip.show()
+
+    def mask_stripe(self):
+        self.led_gradient()
+        if self.anim_count > 0:
+            self.strip.set_pixel_line(0, self.anim_count - 1 , color_default)
+        if self.anim_count < self.num_pix - 2:
+            self.strip.set_pixel_line(self.anim_count + 3, self.num_pix, color_default)
+    
+    def anim_step(self):
+        print("Alt -> " + str(self.anim_count))
+        if self.anim_enable:
+            if self.direction:
+                if self.anim_count < self.num_pix - 1:
+                    self.anim_count += 1
+                else:
+                    self.anim_count = 0
+            else:
+                if self.anim_count > 0:
+                    self.anim_count -= 1
+                else:
+                    self.anim_count = self.num_pix - 1
+        #print("Neu -> " + str(self.anim_count))
+    
+    def make_anim(self):
+        if self.anim_type == "Pixel":
+            self.led_fill()
+            self.led_set_pixel()
+        self.anim_step()
+
+
+
+led_1 = LED_STRIP(20, 0, 2, color_red, color_yellow, True)
+
+
+#led_1.strip_fill()
+led_1.led_gradient()
+led_1.led_show()
+
+time.sleep(2)
+
+while(True):
+    led_1.make_anim()
+    led_1.led_show()
+    time.sleep(0.1)
+
 
