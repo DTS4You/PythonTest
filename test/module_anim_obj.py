8e971955f3914a938aaf49bfae5360f41f67a26e
DTS4You <info@datentechnikschwarz.de>
2026-08-19 15:16:44 +0200

    Init

--- test/module_anim_obj.py
+++ test/module_anim_obj.py
@@ -1,32 +1,56 @@
+###############################################################################
+### Module: module_anim_obj.py
+###############################################################################
 class ANIM_OBJ:
     def __init__(self, stripe, start, lenght, pattern):
         self.stripe     = stripe
         self.start      = start
         self.lenght     = lenght
         self.pattern    = pattern
+        self.position   = 0
+        self.modifyed   = False
 
 
 class ANIM_PATTERN:
-    def __init__(self, led_pattern, color_index_1, color_index_2, color_index_3):
+    def __init__(self, led_pattern, color_index_1, color_index_2=None, color_index_3=None):
         self.led_pattern    = led_pattern
         self.color_index_1  = color_index_1
         self.color_index_2  = color_index_2
         self.color_index_3  = color_index_3
-        self.pattern_lenght = len(self.led_pattern)
+        self.lenght         = len(self.led_pattern)
 
 
 
-anim_pattern = ANIM_PATTERN([1,2,3,2,1],11,12,13)
 
-print("Start")
 
-print(anim_pattern.pattern_lenght)
+def main():
 
-anim_obj = []
+    anim_pattern_1 = ANIM_PATTERN([1,2,3,2,1],11,12,13)
+    anim_pattern_2 = ANIM_PATTERN([1,2],5,8)
 
-anim_obj.append(ANIM_OBJ(1,1,10,anim_pattern))
+    print("--- Start ---")
 
-print("Objekt")
+    anim_obj = []
 
-print(anim_obj[0].pattern.color_index_1)
+    print("Objekte erzeugen")
+    anim_obj.append(ANIM_OBJ(1,1,10,anim_pattern_1))
+    anim_obj.append(ANIM_OBJ(2,1,10,anim_pattern_2))
+
+    anim_obj[1].modifyed = True
+
+    print("Anzahl der Anim_Objekte:", len(anim_obj))
+    print(anim_obj[0].pattern.lenght)
+    print(anim_obj[1].pattern.lenght)
+
+    for i in range(len(anim_obj)):
+        print("Objekt:", i ,"Modified:", anim_obj[i].modifyed)
+    print("--- Ende ---")
+
+
+#------------------------------------------------------------------------------
+#--- Main
+#------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
 
